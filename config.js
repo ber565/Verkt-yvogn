693a43742039aacdd63c1ece3d994c2da9e7af9b
ber565 <erik@bertils.org>
2026-02-10 20:36:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-
+https://oaekqkpkwzopodzoryyp.supabase.co
+sb_publishable_N2BNV3jxI-EXY3Z0kDELpw_E4sprVNp
 // Sett base-URL til API. Når frontend serveres fra samme Express-app, bruk relativ sti.
 const API_BASE = '';
